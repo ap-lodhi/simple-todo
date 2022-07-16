a5a0ea31d0eb429b0fa3a549fd32bf51ee38b403
ap-lodhi <anilpatel.bcs@gmail.com>
2022-07-16 18:21:48 +0530

    todo

---
+++ src/Components/List.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+const List =(props)=>{
+
+    return (
+        <>
+        <div className="todo_style">
+       <button 
+       onClick={()=>{
+        props.onSelect(props.id);
+       }}>
+        x
+        </button> 
+        <li>{props.text}</li>
+
+        </div>
+        </>
+
+    )
+    
+      
+    
+    }
+export default List;
+ 
---
+++ src/Components/todo.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useState } from "react";
+import List from "./List";
+
+
+const Todo=()=>{
+    const [inputList , setInputList]=useState("")
+    const [item, setItem] =useState([]);
+    const itemEvent =(e)=>{
+        setInputList(e.target.value)
+    }
+    const handleAdd=()=>{
+
+        setItem((old)=>{
+            return[...old, inputList]
+        })
+        setInputList("");
+    }
+    
+    const deleteitem = (id)=>{
+        console.log('deleted')
+        setItem((old)=>{
+            return old.filter((ele, ind)=>{
+                return ind !== id;
+            });
+        });
+    }
+    return(
+        <>
+        <div className="main_div">
+        <div className="center_div">
+            <br />
+            <h1>TODO List</h1>
+            <br />
+            <input type='text' placeholder='Add a Items' onChange={itemEvent}
+            value={inputList}/>
+            <button onClick={handleAdd}>+</button>
+
+            <ol>
+                {item.map((val, ind)=>{
+                  return  <List  text={val}
+                                key={ind}
+                                id={ind}
+                                onSelect={deleteitem}
+                  />
+                } )}
+                
+            </ol>
+        </div>
+        
+        </div>
+        </>
+    )
+}
+
+export default Todo;
